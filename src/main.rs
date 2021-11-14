e28b6aeb002f032c6a9e03a24f3044d2bd356d27
Fabian Vilers <fabian.vilers@dev-one.com>
2021-11-14 09:50:27 +0100

    Accept requests and send dummy response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: use configured address and port instead of 127.0.0.1:8080
+    // TODO: gracefully handle error instead of unwrap()
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        // TODO: gracefully handle error instead of unwrap()
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // TODO: handle requests of size over 1024 bytes
+    let mut buffer = [0; 1024];
+
+    // TODO: gracefully handle error instead of unwrap()
+    stream.read(&mut buffer).unwrap();
+
+    let contents = "Hello world!";
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    // TODO: gracefully handle error instead of unwrap()
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
